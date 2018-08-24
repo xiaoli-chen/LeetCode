bccc0674d6cac75397703de10e9f4afb517ed1e2
Xiaoli Chen <haier.cxl@gmail.com>
2018-08-24 11:17:59 -0700

    Update 173 Binary Search Tree Iterator.py

--- 173 Binary Search Tree Iterator.py
+++ 173 Binary Search Tree Iterator.py
@@ -5,7 +5,7 @@ Calling next() will return the next smallest number in the BST.
 
 Note: next() and hasNext() should run in average O(1) time and uses O(h) memory, where h is the height of the tree.
 """
-__author__ = 'Daniel'
+__author__ = 'Xiaoli'
 
 
 class TreeNode:
@@ -20,24 +20,26 @@ class BSTIterator:
         """
         :type root: TreeNode
         """
-        self.cur = root
-        self.stk = []
+        self.current = root
+        self.stack = []
 
     def hasNext(self):
         """
         :rtype: bool
         """
-        return self.cur or self.stk
+        return self.current or self.stack
 
     def next(self):
         """
         :rtype: int
         :return: the next smallest number
         """
-        while self.cur:
-            self.stk.append(self.cur)
-            self.cur = self.cur.left
+        while self.current:
+            self.stack.append(self.current)
+            self.current = self.current.left
 
-        nxt = self.stk.pop()
-        self.cur = nxt.right
-        return nxt.val
+        next_node = self.stack.pop()
+        self.current = next_node.right
+        return next_node.val
+
+
